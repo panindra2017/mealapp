9480a1117b07de1c4c58cf40c4ee757901a457c2
panindra2017 <30747713+panindra2017@users.noreply.github.com>
2020-12-12 23:24:34 +0530

    intial setup done

---
+++ components/header/TopHeader.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const TopHeader = () => {
+    return (
+        <div className="flex flex-wrap">
+            <div className="px-4"><img src="/logo.svg" style={{width:"200px"}}/></div>
+            <div className="flex-1"> LOCation</div>
+        </div>
+    )
+}
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import { AppLayout } from '../layout/AppLayout'
+ 
+
+export default function Home() {
+  return (
+    <AppLayout><div className="text-xl">  MEAL APPA </div></AppLayout>
+    
+  )
+}
