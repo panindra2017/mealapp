177191bf7f757f75a039e70268ccb22ca08341d5
panindra2017 <30747713+panindra2017@users.noreply.github.com>
2020-12-12 23:34:29 +0530

    comiint header

---
+++ components/Location/TopLocationHolder.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+const TopLocationHolder = (props: Props) => {
+    return (
+        <div className="w-full flex">
+            <div>  </div>  <div> Banashankari, Bangalore</div>
+        </div>
+    )
+}
+
+export default TopLocationHolder
--- components/header/TopHeader.tsx
+++ components/header/TopHeader.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import TopLocationHolder from '../Location/TopLocationHolder'
 
 export const TopHeader = () => {
     return (
-        <div className="flex flex-wrap">
-            <div className="px-4"><img src="/logo.svg" style={{width:"200px"}}/></div>
-            <div className="flex-1"> LOCation</div>
+        <div className="flex flex-wrap border-b  shadow w-full py-2">
+            <div className="px-4"><img src="/logo.svg" style={{width:"110px"}}/></div>
+            <div className="flex-1   pl-auto  text-sm "> <TopLocationHolder/></div>
         </div>
     )
 }
