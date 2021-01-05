d08153191a3aee375665036bcdb8af2b7d497de8
panindra2017 <30747713+panindra2017@users.noreply.github.com>
2021-01-05 17:14:45 +0530

    test

--- components/Subscribe/PayOutButton.tsx
+++ components/Subscribe/PayOutButton.tsx
@@ -9,7 +9,7 @@ const PayoutButton = (props: any) => {
 
     return (
         <div className="flex flex-wrap ">
-            <button className="flex-1 py-2 bg-gray--800 text-white px-16 rounded-xl w-full " onClick={(e: any) => { props.onSubmit(e) }}>PayNow </button>
+            <button className="flex-1 py-2 bg-gray-800 text-white px-16 rounded-xl w-full " onClick={(e: any) => { props.onSubmit(e) }}>PayNow </button>
         </div>
     )
 }
